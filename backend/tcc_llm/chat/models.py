a9532a81be91b3d6273cb11a84ad6e7c58079b2c
ViniciusAriel <viniciusarielarruda@tutanota.com>
2024-09-08 18:16:27 -0300

    Run migrations for creating models into the actual data base

--- backend/tcc_llm/chat/models.py
+++ backend/tcc_llm/chat/models.py
@@ -11,12 +11,18 @@ class ChatUser(models.Model):
     
 class Chat(models.Model):
     user_id = models.PositiveBigIntegerField(unique=True)
-    LLM = models.CharField(max_length=255, null=False)
-    Title = models.CharField(max_length=255)
-    Date = models.DateField()
+    llm = models.CharField(max_length=255, null=False)
+    title = models.CharField(max_length=255)
+    date = models.DateField()
 
-class Message(models.Model)
+    def __str__(self):
+        return str(self.title)
+
+class Message(models.Model):
     chat_id = models.PositiveBigIntegerField()
     content = models.CharField(max_length=1024)
     sender_is_llm = models.BooleanField(blank=False)
-    Date = models.DateField()
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.content)
