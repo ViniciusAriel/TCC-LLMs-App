038a0df957ec913e866df80160d0c69d208f919f
ViniciusAriel <viniciusarielarruda@tutanota.com>
2024-09-20 07:52:27 -0300

    sets up mistral ai api calls

--- backend/tcc_llm/chat/utils.py
+++ backend/tcc_llm/chat/utils.py
@@ -3,28 +3,36 @@ from datetime import datetime
 import os
 from dotenv import load_dotenv, find_dotenv
 
-# from langchain_community.chat_models import ChatOpenAI
-from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 
+from langchain_openai import OpenAI
 from langchain_ollama.chat_models import ChatOllama
+from langchain_mistralai.chat_models import ChatMistralAI
 
 _ = load_dotenv(find_dotenv())
 
-chat_open_ai = ChatOpenAI(temperature=0.0, model="gpt-3.5-turbo")
-
+openai_key = os.getenv('OPENAI_API_KEY')
 llama_key = os.getenv('LLAMA_API_KEY')
+mistral_key = os.getenv('MISTRAL_API_KEY')
+
+chat_openai = OpenAI(model="gpt-3.5-turbo", api_key=openai_key)
 chat_ollama = ChatOllama(model="llama3.1", api_key=llama_key)
+chat_mistral = ChatMistralAI(api_key=mistral_key)
+
+template_string = """{text}"""
+prompt_template = ChatPromptTemplate.from_template(template_string)
+
+openai_chain = prompt_template | chat_openai
+llama_chain = prompt_template | chat_ollama
+mistral_chain = prompt_template | chat_mistral
 
 def get_chat_response(prompt, chat_id):
-        template_string = """{text}"""
-
-        prompt_template = ChatPromptTemplate.from_template(template_string)
         llm_prompt = prompt_template.format_messages(text=prompt)
 
-        # chat_response = chat_open_ai(prompt)
-        # chat_response = chat_ollama(llm_prompt)
-        chat_response = "Opa meu patrão"
+        # chat_response = openai_chain.invoke(llm_prompt).content
+        # chat_response = llama_chain.invoke(llm_prompt).content
+        chat_response = mistral_chain.invoke(llm_prompt).content
+
         chat_message = Message()
         chat_message.chat = Chat.objects.filter(pk=chat_id).first()
         chat_message.content = chat_response
