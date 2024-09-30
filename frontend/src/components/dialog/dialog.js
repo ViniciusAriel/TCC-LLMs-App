227f1de8339a78182baef63b784b9d2b63014759
asakuraLie <lie.asakura000@gmail.com>
2024-09-30 12:00:21 -0300

    ✨ Chat Itself

--- frontend/src/components/dialog/dialog.js
+++ frontend/src/components/dialog/dialog.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
 
-import './dialog.css'
+import TextBubble from "../textbubble/textBubble.js";
+
+import "./dialog.css";
 
 export default function Dialog() {
-  return (
-    <div className='dialog-container'>dialog</div>
-  )
+    const messages = [
+        {
+            fromChat: true,
+            content:
+                "CHAT 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec augue enim. Phasellus venenatis turpis at velit aliquet fringilla. Quisque aliquet euismod elit a molestie. In semper vestibulum aliquam. Suspendisse purus metus, consequat et mauris et, mattis laoreet mi. Nunc felis metus, rutrum in cursus hendrerit, bibendum quis eros. Phasellus nec pretium orci. Sed vitae dolor vulputate, venenatis tortor ac, ultrices est.",
+        },
+        {
+            fromChat: false,
+            content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec augue enim. Phasellus venenatis turpis at velit aliquet fringilla. Quisque aliquet euismod elit a molestie. In semper vestibulum aliquam. Suspendisse purus metus, consequat et mauris et, mattis laoreet mi. Nunc felis metus, rutrum in cursus hendrerit, bibendum quis eros. Phasellus nec pretium orci. Sed vitae dolor vulputate, venenatis tortor ac, ultrices est.",
+        },
+        {
+            fromChat: true,
+            content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec augue enim. Phasellus venenatis turpis at velit aliquet fringilla. Quisque aliquet euismod elit a molestie. In semper vestibulum aliquam. Suspendisse purus metus, consequat et mauris et, mattis laoreet mi. Nunc felis metus, rutrum in cursus hendrerit, bibendum quis eros. Phasellus nec pretium orci. Sed vitae dolor vulputate, venenatis tortor ac, ultrices est.",
+        },
+        {
+            fromChat: false,
+            content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec augue enim. Phasellus venenatis turpis at velit aliquet fringilla. Quisque aliquet euismod elit a molestie. In semper vestibulum aliquam. Suspendisse purus metus, consequat et mauris et, mattis laoreet mi. Nunc felis metus, rutrum in cursus hendrerit, bibendum quis eros. Phasellus nec pretium orci. Sed vitae dolor vulputate, venenatis tortor ac, ultrices est.",
+        },
+        {
+            fromChat: true,
+            content:
+                "CHAT 5: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec augue enim. Phasellus venenatis turpis at velit aliquet fringilla. Quisque aliquet euismod elit a molestie. In semper vestibulum aliquam. Suspendisse purus metus, consequat et mauris et, mattis laoreet mi. Nunc felis metus, rutrum in cursus hendrerit, bibendum quis eros. Phasellus nec pretium orci. Sed vitae dolor vulputate, venenatis tortor ac, ultrices est.",
+        },
+        {
+            fromChat: false,
+            content:
+                "USER: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec augue enim. Phasellus venenatis turpis at velit aliquet fringilla. Quisque aliquet euismod elit a molestie. In semper vestibulum aliquam. Suspendisse purus metus, consequat et mauris et, mattis laoreet mi. Nunc felis metus, rutrum in cursus hendrerit, bibendum quis eros. Phasellus nec pretium orci. Sed vitae dolor vulputate, venenatis tortor ac, ultrices est.",
+        },
+        {
+            fromChat: true,
+            content:
+                "CHAT: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec augue enim. Phasellus venenatis turpis at velit aliquet fringilla. Quisque aliquet euismod elit a molestie. In semper vestibulum aliquam. Suspendisse purus metus, consequat et mauris et, mattis laoreet mi. Nunc felis metus, rutrum in cursus hendrerit, bibendum quis eros. Phasellus nec pretium orci. Sed vitae dolor vulputate, venenatis tortor ac, ultrices est.",
+        },
+        { fromChat: false, content: "USER: Lorem ipsum dolor sit amet" },
+        { fromChat: true, content: "CHAT: Lorem ipsum dolor sit amet" },
+        { fromChat: false, content: "USER: Lorem ipsum dolor sit amet" },
+        { fromChat: true, content: "CHAT: Lorem ipsum dolor sit amet" },
+        { fromChat: false, content: "USER: Lorem ipsum dolor sit amet" },
+    ];
+
+    return (
+        <div className="dialog-container">
+            {messages.map((message) => {
+                return <TextBubble message={message} />;
+            })}
+        </div>
+    );
 }
--- frontend/src/components/textbubble/textBubble.js
+++ frontend/src/components/textbubble/textBubble.js
@@ -2,6 +2,18 @@ import React from "react";
 
 import "./textBubble.css";
 
-export default function TextBubble() {
-    return <div>textBubble</div>;
+export default function TextBubble({ message }) {
+    return (
+        <div className="textbubble-container">
+            {!(message.fromChat) ? (
+                <div className="user-message">
+                    <p>USER: {message.content}</p>
+                </div>
+            ) : (
+                <div className="chat-message">
+                    <p>CHAT:{message.content}</p>
+                </div>
+            )}
+        </div>
+    );
 }
